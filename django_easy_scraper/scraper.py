9ca1336b99294c22a5bfdf771fdbdd84e512d74d
dearopen <mamundev2019@gmail.com>
2020-11-23 19:15:59 +0600

    Added mechanism for xpath

--- django_easy_scraper/scraper.py
+++ django_easy_scraper/scraper.py
@@ -1,7 +1,10 @@
 import re
 import csv
 from html import unescape
+
 import requests
+
+from lxml import html
 
 
 def get_page_content(url):
@@ -15,6 +18,13 @@ class Scraper(object):
     """
     regex_fields = {}
 
+
+    """
+    xpath_fields will take following items - 
+        fields_name: '<regex pattern>'
+    """
+    xpath_fields = {}
+
     @classmethod
     def regex_url_scraper(self, url, raise_exception=False):
         content = get_page_content(url)
@@ -22,6 +32,27 @@ class Scraper(object):
         scrapped = {}
         for field, pattern in self.regex_fields.items():
             result = re.findall(pattern, content)
+            if len(result) == 0:
+                if raise_exception:
+                    raise LookupError(field)
+                else:
+                    scrapped[field] = ''
+            else:
+                scrapped[field] = result[0]
+
+        return scrapped
+
+
+    @classmethod
+    def xpath_scraper(self, url, raise_exception=False):
+        content = get_page_content(url)
+
+        tree = html.fromstring(content)
+
+        scrapped = {}
+        for field, pattern in self.xpath_fields.items():
+            result = tree.xpath(pattern)
+
             if len(result) == 0:
                 if raise_exception:
                     raise LookupError(field)
